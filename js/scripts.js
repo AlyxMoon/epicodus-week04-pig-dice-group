373404d7a0e2eb45cb55b4e34c57156cdf8b54d1
AlyxMoon <allisterkmoon@gmail.com>
2021-06-16 11:44:10 -0700

    add a score tracker function
    Co-authored-by: Kyle Williamson <kylewilliamsonv1@gmail.com>
    Co-authored-by: Arthur Lee <meleearthur@gmail.com>
    Co-authored-by: Gurshan Kaile <gurshankaile206@gmail.com>
    Co-authored-by: Scott Hutley <scotthutley1@comcast.net>
    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,34 @@ function diceRoll () {
   return Math.ceil((Math.random() * 6));
 };
 
+function checkIfTurnOver (roll) {
+  if (roll === 1) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function scoreTracker () {
+  let currentScore = 0
+
+  for(let i = 0; i < 5; i++) {
+    let playerOneRoll = diceRoll(); 
+    
+    if (checkIfTurnOver(playerOneRoll)){
+      currentScore = 0
+      console.log("You rolled a 1")
+      break
+    } else {
+      currentScore += playerOneRoll
+    }
+    console.log('playeroneroll', playerOneRoll)
+    console.log('currentscore', currentScore)
+  }
+
+  console.log('end score', currentScore)
+};
+
 function runTests () {
   console.log('diceRoll returns something between 1 and 6')
 
@@ -24,6 +52,12 @@ function runTests () {
   if (!gotWrongAnswer) {
     console.log('success: it works!')
   }
+
+  console.log('checkIfTurnOver(1)', checkIfTurnOver(1), 'expect this to be true')
+  console.log('checkIfTurnOver(2)', checkIfTurnOver(2), 'expect this to be false')
+  console.log('checkIfTurnOver(5)', checkIfTurnOver(5), 'expect this to be false')
+  console.log('checkIfTurnOver(6)', checkIfTurnOver(6), 'expect this to be false')
 }
 
 runTests()
+scoreTracker()
