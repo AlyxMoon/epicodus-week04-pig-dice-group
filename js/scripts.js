6f8b781a09dea651a1761b52dc3af0ceb67a776a
AlyxMoon <allisterkmoon@gmail.com>
2021-06-16 10:34:15 -0700

    fill out the diceRoll function
    Co-authored-by: Kyle Williamson <kylewilliamsonv1@gmail.com>
    Co-authored-by: Arthur Lee <meleearthur@gmail.com>
    Co-authored-by: Gurshan Kaile <gurshankaile206@gmail.com>
    Co-authored-by: Scott Hutley <scotthutley1@comcast.net>
    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
 console.log('stuff happens!')
+
+function diceRoll () {
+  return Math.ceil((Math.random() * 6));
+};
+
+function runTests () {
+  console.log('diceRoll returns something between 1 and 6')
+
+  let gotWrongAnswer = false
+
+  for (let i = 0; i < 1000; i++) {
+    const result = diceRoll()
+
+    // check if result is within our bounds, 1 - 6
+    // but can be random, so we can't check for a specific number
+    if (result >= 7 || result <= 0) {
+      gotWrongAnswer = true
+      console.log('WRONG!, digit out of bounds!', result)
+      break
+    }
+  }
+
+  if (!gotWrongAnswer) {
+    console.log('success: it works!')
+  }
+}
+
+runTests()
